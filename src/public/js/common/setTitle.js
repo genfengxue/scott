664f8f5478d292855f9fe8eba17e2a267454d7e5
kaiqigong <cagegong@gmail.com>
2016-02-04 01:19:43 +0800

    not display ico

--- src/public/js/common/setTitle.js
+++ src/public/js/common/setTitle.js
@@ -3,6 +3,7 @@ export default (title) => {
   document.title = title; // hack在微信等webview中无法修改document.title的情况
   const $iframe = document.createElement('iframe');
   $iframe.src = '/favicon.ico';
+  $iframe.style.display = 'none';
   $iframe.onload = () => {
     setTimeout(() => {
       $iframe.onload = null;
