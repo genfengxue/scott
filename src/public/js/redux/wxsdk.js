4679dd53bb4ff0f57f0357acfd8fd5a7140e10dc
kaiqigong <cagegong@gmail.com>
2016-03-04 17:51:35 +0800

    remove log

--- src/public/js/redux/wxsdk.js
+++ src/public/js/redux/wxsdk.js
@@ -19,7 +19,7 @@ export const fetchSignatureAsync = () => {
     const response = await ajax.get('/api/wechat/signature/');
     if (response) {
       wx.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: response.appId, // 必填，公众号的唯一标识
         timestamp: response.timestamp, // 必填，生成签名的时间戳
         nonceStr: response.nonceStr, // 必填，生成签名的随机串
