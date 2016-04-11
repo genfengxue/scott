837080d1f7a4d734518f620b7857c4d4d6600c82
Bruce Chen <bruce@beansmile.com>
2016-04-11 21:36:16 +0800

    发音语调朗读打boss设计还原

--- src/public/js/views/PronunciationLessonActivityView.js
+++ src/public/js/views/PronunciationLessonActivityView.js
@@ -33,8 +33,10 @@ class PronunciationLessonActivityView extends Component {
           <p className="course-label">请听讲解</p>
         </div>
         <div className="course-content">
+          {/* TODO: This is a placeholder, should be replaced with video */}
           <img src="http://placehold.it/336x188" />
 
+          {/* TODO: Listen */}
           <div className="listen-explain">
             <h6 className="pronunciation-symbol">cop [a]</h6>
             <ol>
@@ -44,16 +46,36 @@ class PronunciationLessonActivityView extends Component {
               <li>这个音典型错误是读成了 这个音类似于你去看医生,医生让你张大嘴说 "啊", 这个音典型错误是读成了 这个音类似于你去看医生,医生让你张大嘴说 "啊" </li>
             </ol>
           </div>
+          {/* TODO: Reading */}
+          <div className="reading-pronunciation">
+            <h6>her here hair hire</h6>
+            <p>[her][hir][her][hear]</p>
+          </div>
 
-           <div className="reading-pronunciation">
-              <h6>her here hair hire</h6>
-              <p>[her][hir][her][hear]</p>
-           </div>
+          {/* TODO:  Fight boss */}
+          <div className="fight-boss">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+
+            <form>
+              <div className="form-group">
+                <label>昵称</label>
+                <input type="text" className="name"/>
+              </div>
+              <div className="form-group">
+                <label>时间</label>
+                <input type="text" className="time" placehold="分钟" />
+              </div>
+            </form>
+          </div>
         </div>
+
         <div className="course-buttons">
-          <span className="boss-button"></span>
-          <span className="sound-button"></span>
-          <span className="next-button"></span>
+          <span className="boss-button hidden"></span>
+          <span className="sound-button hidden"></span>
+          <span className="next-button hidden"></span>
+          <span className="record-button hidden"></span>
+          <span className="submit-button"></span>
+          <span className="upload-button"></span>
         </div>
       </div>
     );
