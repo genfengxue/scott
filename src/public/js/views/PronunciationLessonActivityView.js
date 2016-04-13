6b60b7a87140deacc54de9e63b943d9660dd5a07
Bruce Chen <bruce@beansmile.com>
2016-04-13 21:10:37 +0800

    使用react slick组件

--- src/public/js/views/PronunciationLessonActivityView.js
+++ src/public/js/views/PronunciationLessonActivityView.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import {actions} from '../redux/pronunciationLessonActivity';
 import {Link} from 'react-router';
-import setTitle from '../common/setTitle';
+import Slider from 'react-slick';
 
 const mapStateToProps = ({pronunciationLessonActivity}) => ({
   pronunciationLessonActivity,
@@ -24,67 +24,105 @@ class PronunciationLessonActivityView extends Component {
   render() {
     const {docs} = this.props.pronunciationLessonActivity;
     window.pronunciationLessonActivity = this.props.pronunciationLessonActivity;
+
+    const CustomPrevArrow = React.createClass({
+      render() {
+        return (
+          <div className="slick-prev" {...this.props}><icon className="prev-button"></icon></div>
+        );
+      },
+    });
+
+    const CustomNextArrow = React.createClass({
+      render() {
+        return (
+          <div className="slick-next" {...this.props}><icon className="next-button"></icon></div>
+        );
+      },
+    });
+
+    const settings = {
+      dots: false,
+      swipe: false,
+      touchMove: false,
+      prevArrow: CustomPrevArrow,
+      nextArrow: CustomNextArrow,
+    };
+
     return (
       <div className="pronunciation-activity-view">
         <nav className="navbar">
           <ul className="nav navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to={`/home/pronunciation_courses/`}>
+              <Link className="nav-link" to={`/home/pronunciation_courses/2/lessons/`}>
                 <i className="icon-left" />
               </Link>
             </li>
           </ul>
         </nav>
-        <div className="course-info">
 
-          <img src="http://7xr387.com1.z0.glb.clouddn.com/windwind.jpg" className="mentor-photo" />
-          <p className="course-label">请听讲解</p>
-        </div>
-        <div className="course-content">
-          {/* TODO: This is a placeholder, should be replaced with video */}
-          <img src="http://placehold.it/336x188" />
-
+        <Slider {...settings}>
           {/* TODO: Listen */}
-          <div className="listen-explain">
-            <h6 className="pronunciation-symbol">cop [a]</h6>
-            <ol>
-              <li>[a]这个音标键盘上打不出来,键盘上能出来是这样的 :a</li>
-              <li>这个音类似于你去看医生, 医生让你张大嘴说 "啊"</li>
-              <li>这个音标在发音时比较靠后</li>
-              <li>这个音典型错误是读成了 这个音类似于你去看医生,医生让你张大嘴说 "啊", 这个音典型错误是读成了 这个音类似于你去看医生,医生让你张大嘴说 "啊" </li>
-            </ol>
+          <div className="activity-item">
+            <div className="course-info">
+              <img src="http://7xr387.com1.z0.glb.clouddn.com/windwind.jpg" className="mentor-photo" />
+              <p className="course-label">请听讲解</p>
+            </div>
+            <div className="course-content">
+              {/* TODO: This is a placeholder, should be replaced with video */}
+              <img src="http://placehold.it/335x188" />
+
+              <div className="listen-explain">
+                <h6 className="pronunciation-symbol">cop [a]</h6>
+                <ol>
+                  <li>[a]这个音标键盘上打不出来,键盘上能出来是这样的 :a</li>
+                  <li>这个音类似于你去看医生, 医生让你张大嘴说 "啊"</li>
+                  <li>这个音标在发音时比较靠后</li>
+                  <li>这个音典型错误是读成了 这个音类似于你去看医生,医生让你张大嘴说 "啊", 这个音典型错误是读成了 这个音类似于你去看医生,医生让你张大嘴说 "啊" </li>
+                </ol>
+              </div>
+            </div>
           </div>
+
           {/* TODO: Reading */}
-          <div className="reading-pronunciation">
-            <h6>her here hair hire</h6>
-            <p>[her][hir][her][hear]</p>
+          <div className="activity-item">
+            <div className="course-info">
+              <img src="http://7xr387.com1.z0.glb.clouddn.com/windwind.jpg" className="mentor-photo" />
+              <p className="course-label">请朗读整段文本</p>
+            </div>
+            <div className="course-content">
+              <div className="reading-pronunciation">
+                <h6>her here hair hire</h6>
+                <p>[her][hir][her][hear]</p>
+              </div>
+            </div>
           </div>
 
           {/* TODO:  Fight boss */}
-          <div className="fight-boss">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+          <div className="activity-item">
+            <div className="course-info">
+              <img src="http://7xr387.com1.z0.glb.clouddn.com/windwind.jpg" className="mentor-photo" />
+              <p className="course-label">请朗读</p>
+            </div>
+            <div className="course-content">
+              <div className="fight-boss">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
 
-            <form>
-              <div className="form-group">
-                <label>昵称</label>
-                <input type="text" className="name"/>
+                <form>
+                  <div className="form-group">
+                    <label>昵称</label>
+                    <input type="text" className="name"/>
+                  </div>
+                  <div className="form-group">
+                    <label>时间</label>
+                    <input type="text" className="time" placehold="分钟" />
+                  </div>
+                </form>
               </div>
-              <div className="form-group">
-                <label>时间</label>
-                <input type="text" className="time" placehold="分钟" />
-              </div>
-            </form>
+            </div>
           </div>
-        </div>
+        </Slider>
 
-        <div className="course-buttons">
-          <span className="boss-button"></span>
-          <span className="sound-button"></span>
-          <span className="next-button"></span>
-          <span className="record-button hidden"></span>
-          <span className="submit-button hidden"></span>
-          <span className="upload-button hidden"></span>
-        </div>
       </div>
     );
   }
